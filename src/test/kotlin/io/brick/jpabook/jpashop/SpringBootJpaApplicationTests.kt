1c9379be9c3505d68870066811767cb4fec0700e
beaniejoy <lhb1025@gmail.com>
2022-07-05 21:06:48 +0900

    project initial setting

---
+++ src/test/kotlin/io/brick/jpabook/jpashop/SpringBootJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.brick.jpabook.jpashop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
