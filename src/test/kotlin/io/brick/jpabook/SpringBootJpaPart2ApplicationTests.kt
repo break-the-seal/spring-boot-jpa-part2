978df618c9017efcc3385412f871f8bfb945ec46
beaniejoy <lhb1025@gmail.com>
2022-12-04 23:57:41 +0900

    project setting

---
+++ src/test/kotlin/io/brick/jpabook/SpringBootJpaPart2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.brick.jpabook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootJpaPart2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
